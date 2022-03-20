34b946a5fc1ad1002bff5e52ee572789909c1936
Ishmeet Singh Saggu <57167867+Psychik-N@users.noreply.github.com>
2022-03-20 22:25:57 +0530

    Update ColumnFloat.cpp

--- ColumnFloat.cpp
+++ ColumnFloat.cpp
@@ -7,10 +7,29 @@ ColumnFloat::ColumnFloat(string name):Column(name){}
 ColumnFloat::~ColumnFloat(){}
 
 void ColumnFloat::writeFile(){
-
+    ofstream writeptr(file_source, ios::out | ios::binary);
+    if(!writeptr) {
+        cout << "Cannot open the file" << endl;
+        return;
+    }
+    for(int i = 0; i < column.size(); i ++) {
+        writeptr.write((char *)column[i]. sizeof(Data<float>));
+    }
+    writeptr.close();
 }
 void ColumnFloat::loadFile(){
-
+    ifstream readptr(file_source, ios::in | ios::binary);
+    if(!readptr) {
+        cout << "Cannot open the file" << endl;
+        return;
+    }
+    readptr.seekg(0, ios::beg);
+    while(readptr.peek() != EOF) {
+        Data<float> *ptr = new Data<float>();
+        readptr.read((char*)ptr, sizeof(Data<float>));
+        column.push_back(ptr);
+    }
+    readptr.close();
 }
 
 void ColumnFloat::insertValue(float value){
