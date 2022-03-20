761147aca8f95849d891c08240d3f1063885909f
Prajwal7842 <34996624+Prajwal7842@users.noreply.github.com>
2022-03-20 21:01:28 +0530

    Table files added (#2) (#1)
    * table files updated
    
    * Table files updated
    
    * Table files updated
    
    * TableRecord files added
    
    Co-authored-by: Sahaj8 <43809376+Sahaj8@users.noreply.github.com>

--- Table.h
+++ Table.h
@@ -1,30 +1,35 @@
 #pragma once
 #include <bits/stdc++.h>
 #include "Column.h"
+#include "ColumnRecord.h"
 
 using namespace std;
 
 class Table{
 	private:
-		string name;
+		string name, address;
 		vector<Column * > columns;
 		Column * primaryKey;
 		unordered_set<string >columnNames;
+		vector<ColumnRecord*> ColumnRecords;
 		// vector<pair<Column *,pair<Table *, Column *>>> foreignKey; 
 	public:
 		Table();
-		Table(string name);
+		Table(string name, string address);
 		// Table(string name,vector<Column *> columns);
 		// Table(string name,vector<Column *> columns, Column * primaryKey);
 		~Table();
 
+		void loadFile();
+		void writeFile();
+
 		void dropTable();
-		void addColumn(Column * column);
+		void addColumn(Column * column, string type);
 		void dropColumn(string columnName);
-		void alterColumn(/*Old column new column new datatype*/); 
+		void alterColumn(string oldName, string newName); 
 		void showTable(vector<Column *>& columns/*, Filters */);
 
-		void setName(string name);
+		void setName(string newName);
 		void setPrimaryKey(Column * column);
 
 		Column * getPrimaryKey();
