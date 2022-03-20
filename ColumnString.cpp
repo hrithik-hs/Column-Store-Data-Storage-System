30ae072cb91c4d966ca918f43e03566b8e1ca5a5
Ishmeet Singh Saggu <57167867+Psychik-N@users.noreply.github.com>
2022-03-20 22:27:20 +0530

    Update ColumnString.cpp

--- ColumnString.cpp
+++ ColumnString.cpp
@@ -7,10 +7,29 @@ ColumnString::ColumnString(string name):Column(name){}
 ColumnString::~ColumnString(){}
 
 void ColumnString::writeFile(){
-
+    ofstream writeptr(file_source, ios::out | ios::binary);
+    if(!writeptr) {
+        cout << "Cannot open the file" << endl;
+        return;
+    }
+    for(int i = 0; i < column.size(); i ++) {
+        writeptr.write((char *)column[i]. sizeof(Data<string>));
+    }
+    writeptr.close();
 }
 void ColumnString::loadFile(){
-
+    ifstream readptr(file_source, ios::in | ios::binary);
+    if(!readptr) {
+        cout << "Cannot open the file" << endl;
+        return;
+    }
+    readptr.seekg(0, ios::beg);
+    while(readptr.peek() != EOF) {
+        Data<string> *ptr = new Data<string>();
+        readptr.read((char*)ptr, sizeof(Data<string>));
+        column.push_back(ptr);
+    }
+    readptr.close();
 }
 
 void ColumnString::insertValue(string value){
