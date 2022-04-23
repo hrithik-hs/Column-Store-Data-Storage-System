bfe1cc846a67fc86feec11ad8cb16c97779559bb
Sahaj8 <sahaj.vaghasiya8112@gmail.com>
2022-04-23 22:44:43 +0530

    create query added

--- main.cpp
+++ main.cpp
@@ -12,13 +12,71 @@
 
 using namespace std;
 
+set<string> databaseNames;
+
+void loadRoot(){
+    string file_source="./File/root";
+    FILE* readptr = fopen(&file_source[0], "r");
+    if(!readptr) {
+        cout << "[ M- ] [ Load root ] Cannot open root file." << endl;
+    }
+    while(1){
+        char readData[100];
+		int sz = fread(&readData, sizeof(readData), 1, readptr);
+        if(!sz) break;
+        databaseNames.insert(string(readData));
+    }
+    fclose(readptr);
+}
+
+void writeRoot(){
+    string file_source="./File/root";
+    FILE* writeptr = fopen(&file_source[0], "r");
+    if(!writeptr) {
+        cout << "[ M- ] [ Write root ] Cannot open root file." << endl;
+    }
+    for(auto st:databaseNames){
+        char writeData[100];
+        strcpy(writeData,st.c_str());
+        fwrite(&writeData,sizeof(writeData),1,writeptr);
+    }
+    fclose(writeptr);
+}
+
 int main(int argc, char *argv[]){
+    loadRoot();
+
     Query *q = new Query(argv[1], argv[2]);
+
+    if(argv[2] == "create") {
+        vector<DatabaseCreate> vecObject = q->parseCreateQuery();
+        for(auto dbCreate: vecObject){
+            string databaseName = dbCreate.databaseName;
+            if(databaseNames.find(databaseName)==databaseNames.end()){
+                string address = "./File/"+databaseName+".db";
+                string folderAddress = "./File/"+databaseName;
+                fs::create_directories(folderAddress);
+                ofstream outfile(address);
+                databaseNames.insert(databaseName);
+            }
+            Database* database = new Database(databaseName, "./File");
+            for(auto tbCreate: dbCreate.tables){
+                string tableName = tbCreate.tableName;
+                database->createTable(tbCreate.tableName);
+                for(auto colCreate: tbCreate.columns){
+                    database->addColumn(tableName,colCreate.columnName,colCreate.columnType);
+                    if(colCreate.isPrimaryConstraint) database->setPrimaryKey(tableName,colCreate.columnName);
+                    else if(colCreate.isUniqueConstraint) database->setIsUniqueConstraint(tableName,colCreate.columnName,true);
+                }
+            }
+        }
+    }
 
     if(argv[2] == "insert") {
         vector<DatabaseInsert> vecObject = q->parseInsertQuery();
         for(auto dbInsert : vecObject) {
             string databaseName = dbInsert.databaseName;
+            if(databaseNames.find(databaseName)==databaseNames.end()) continue;
             Database* database = new Database(databaseName, "./File");
             for(auto tbInsert : dbInsert.tableInserts) {
                 Row* row = new Row();
@@ -45,5 +103,7 @@ int main(int argc, char *argv[]){
             }
         }
     }
+
+    writeRoot();
     return 0;
 }
