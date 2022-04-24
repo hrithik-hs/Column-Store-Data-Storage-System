97e872bf52c73f2caa91fe4421184994d0f2c1a9
Prajwal7842 <prajwalgoel@gmail.com>
2022-04-24 13:12:41 +0530

    Update XML files

--- Input/csvToXml.py
+++ Input/csvToXml.py
@@ -22,14 +22,14 @@ def parse_cust_dimension(row):
     <Column name=\"City\">%s</Column>\n\t \
     <Column name=\"State\">%s</Column>\n\t \
     <Column name=\"Customer_Segment\">%s</Column>\n\t \
-    <\Insert>" % (row.Cust_id, row.Customer_Name, row.City, row.State, row.Customer_Segment))
+    </Insert>" % (row.Cust_id, row.Customer_Name, row.City, row.State, row.Customer_Segment))
 
 def parse_prod_dimension(row):
     return ("<Insert>\n\t \
     <Column name=\"Prod_id\">%s</Column>\n\t \
     <Column name=\"Product_Category\">%s</Column>\n\t \
     <Column name=\"Product_Sub_Category\">%s</Column>\n\t \
-    <\Insert>" % (row.Prod_id, row.Product_Category, row.Product_Sub_Category))
+    </Insert>" % (row.Prod_id, row.Product_Category, row.Product_Sub_Category))
 
 if __name__ == '__main__':
     # Parse market_fact_table.csv
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         f.write("<?xml version=\"1.0\" encoding=\"utf-8\">\n")
         f.write("<root>\n<Database name = \"marketdb\">\n<Table name = \"market_fact_table\">\n")
         f.write('\n'.join(df.apply(parser_market_fact_table, axis=1)))
-        f.write("\n<\Table>\n<\Database>\n<\\root>")
+        f.write("\n</Table>\n</Database>\n</root>")
 
     # Parse cust_dimension.csv
     df = pd.read_csv("cust_dimension.csv")
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         f.write("<?xml version=\"1.0\" encoding=\"utf-8\">\n")
         f.write("<root>\n<Database name = \"marketdb\">\n<Table name = \"cust_dimension\">\n")
         f.write('\n'.join(df.apply(parse_cust_dimension, axis=1)))
-        f.write("\n<\Table>\n<\Database>\n<\\root>")
+        f.write("\n</Table>\n</Database>\n</root>")
 
     # Parse prod_dimension.csv
     df = pd.read_csv("prod_dimension.csv")
@@ -54,5 +54,5 @@ if __name__ == '__main__':
         f.write("<?xml version=\"1.0\" encoding=\"utf-8\">\n")
         f.write("<root>\n<Database name = \"marketdb\">\n<Table name = \"prod_dimension\">\n")
         f.write('\n'.join(df.apply(parse_prod_dimension, axis=1)))
-        f.write("\n<\Table>\n<\Database>\n<\\root>")
+        f.write("\n</Table>\n</Database>\n</root>")
 
